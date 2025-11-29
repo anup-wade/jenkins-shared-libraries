b8586caa14588fd254345d5a693cd4e86eb473b4
anup-wade <71591482+anup-wade@users.noreply.github.com>
2025-11-29 14:44:32 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
-def call(String url, String branch){
-  echo "This is the cloning the code" ,
-  git url: ${url} , branch: ${branch}
-  echo 'coding clone is successfull"
-  
+def call(String url, String branch) {
+    git(
+        url: url,
+        branch: branch
+    )
 }
